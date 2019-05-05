b51c62652d467c999bfbeb53ee37add2ededeb0f
Felix <yinruihu@gmail.com>
2019-05-05 23:02:49 +0100

    Exercise 4

--- Exercise4.hs
+++ Exercise4.hs
@@ -3,11 +3,11 @@ import Data.Char
 import Data.List
 
 
-myWords :: String -> [ String ]
-myWords [ ] = [ ]
-myWords xxs @ (x : xs) | isSpace x = myWords xs
-                         | otherwise = word : myWords rest
-                         where (word, rest) = break isSpace xxs
+words :: String -> [ String ]
+words [ ] = [ ]
+words xxs @ (x : xs) | isSpace x = words xs
+                     | otherwise = word : words rest
+                      where (word, rest) = break isSpace xxs
 
 unWords :: [ String ] -> String
 unWords [ ] = [ ]
